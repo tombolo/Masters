4c40bd3bb8394ec9dc0045d26ebc60b3cb3c11c1
Brian <ndanumumo93@gmail.com>
2025-10-17 15:49:14 +0300

    Initial commit for Masters

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '68794', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '68794',
+    'app.deriv.com': '68794',
+    'staging-app.deriv.com': '68794',
+    'goon-puce.vercel.app': '68794',
+});
+
+export default AppIds;
